79657fdca25ea7405026b5664ca9ee04dfab3f6a
Andrew Wingate <1464801+anfroholic@users.noreply.github.com>
2021-09-18 11:59:07 -0500

    basics working

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 
 
-# import usocket as socket
+
+from gcode_conversion import *  #asyncio does not seem to be able to make outside calls from within handle_client
+
+
 import utime
 from machine import Pin
+import machine
 import network
-# import urequests as requests
 from neopixel import NeoPixel
 import esp
 esp.osdebug(None)
@@ -18,18 +21,6 @@ password = 'AAGI96475'
 port = 80
 loop = asyncio.get_event_loop()
 
-station = network.WLAN(network.STA_IF)
-
-station.active(True)
-station.connect(ssid, password)
-
-while not station.isconnected():
-    print(".", end = "")
-    utime.sleep_ms(250)
-
-print('Connection successful')
-print(station.ifconfig())
-my_ip = station.ifconfig()[0]
 
 # set up pins
 hbt_led = Pin(5, Pin.OUT)
@@ -38,12 +29,30 @@ neo_status = NeoPixel(neo_status_pin, 1)
 func_button = Pin(36, Pin.IN) # Has external pullup
 
 
+#network
+station = network.WLAN(network.STA_IF)
+station.active(True)
+station.connect(ssid, password)
 
+neo_status[0] = (10, 0, 0)
+neo_status.write()
+while not station.isconnected():
+    print(".", end = "")
+    utime.sleep_ms(250)
+
+print('Connection successful')
+print(station.ifconfig())
+my_ip = station.ifconfig()[0]
+neo_status[0] = (0, 10, 0)
+neo_status.write()
+utime.sleep_ms(250)
+neo_status[0] = (0, 0, 0)
+neo_status.write()
 
 
 def web_page():
-  gpio_state = 'some gpio state'
-  json_loaded = 'some json'
+  connect_state = 'fix connection'
+
   html = """
 <html>
     <head>
@@ -57,22 +66,80 @@ def web_page():
     </head>
     <body>
     <h1>Evezor Web Interface</h1>
-    <p>BUS state: <strong>""" + gpio_state + """</strong></p><p><a href="/?led=on"><button class="button">CONNECT</button></a></p>
-    <p><a href="/?led=off"><button class="button button2">DISCONNECT</button></a></p>
-    <p>Map File = <strong>""" + json_loaded + """</strong></p>
-    <p><a href="/?load_json"><button class="button button3">LOAD</button></a></p>
+    <p>Connection State: <strong>""" + connect_state + """</strong></p>
+
+    <p><a href="/?led=off"><button class="button button2">neo off</button></a>          <a href="/?led=on"><button class="button">neo on</button></a></p>
+
+    <p><a href="/?load_json"><button class="button button3">unused</button></a></p>
+    <br><br>
+    <p><strong>Move Machine</strong></p>
+
+    <form action="/move.php">
+    <label for="send_x">X:</label>
+    <input type="text" id="x" name="x"><br><br>
+    <label for="send_y">Y:</label>
+    <input type="text" id="y" name="y"><br><br>
+    <label for="send_z">Z:</label>
+    <input type="text" id="z" name="z"><br><br>
+    <label for="send_a">A:</label>
+    <input type="text" id="a" name="a"><br><br>
+    <input type="submit" value="Submit">
+    </form>
     </body></html>"""
   return html
 
-async def time_check():
-        # do the time check etc here
-        print('Bar started: waiting {}secs')
-        await asyncio.sleep(.1)
+def parse_move(request):
+    end = request.find(' HTTP')
+    action = request[14:end]
+    action += '&'
+    # print('parsing request')
+    # print(action)
+    axes = ['x', 'y', 'z', 'a']
+    parsed = {}
+    for axis in axes:
+        index = action.find('&')
+        if action[2: index] != '':
+            parsed[axis] = action[2: index]
+        action = action[(index + 1):]
+    # print(parsed)
+    parsed['command'] = 'move.linear'
+    parsed['feed'] = 2000
+    return parsed
 
+
+async def buttons():
+    while True:
+        if not func_button.value():
+            print('function button pressed')
+            await asyncio.sleep_ms(250)
+        await asyncio.sleep_ms(50)
+
+async def do_hbt():
+    while True:
+        hbt_led.value(1)
+        await asyncio.sleep(.1)
+        hbt_led.value(0)
+        await asyncio.sleep(.9)
 
 async def handle_client(reader, writer):
-    request = (await reader.read(1024)).decode('utf8')
+    request = (await reader.read(1024)).decode('ascii')
     print(request)
+
+    # process request
+    if request.find('/move') == 4:
+        parsed = parse_move(request)
+        # print(parsed)
+        print(convert(**parsed))
+
+    if request.find('/?led=on') == 4:
+        print('turn led on')
+        neo_status[0] = (0, 25, 0)
+        neo_status.write()
+    if request.find('/?led=off') == 4:
+        print('turn led off')
+        neo_status[0] = (0, 0, 0)
+        neo_status.write()
+
     await writer.awrite(
         b'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nConnection: close\r\n\r\n')
     # with open(web_page(), 'rb') as response:
@@ -86,10 +153,14 @@ async def handle_client(reader, writer):
     await writer.aclose()
     return True
 
-loop.create_task(asyncio.start_server(handle_client, my_ip, port))
-# asyncio.create_task(asyncio.start_server(handle_client, my_ip, port))
 
-# loop.create_task(time_check())
-asyncio.run(time_check())
-print('Time check and async webserver created, listening on {}:{}'.format(my_ip, port))
-loop.run_forever()
+async def main():
+    asyncio.create_task(asyncio.start_server(handle_client, my_ip, port))
+    asyncio.create_task(do_hbt())
+    asyncio.create_task(buttons())
+    while True:
+        await asyncio.sleep(5)
+
+
+while True:
+    asyncio.run(main())
